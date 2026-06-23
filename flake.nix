1bb953407d7b8aececd4c31645760be5188dd674
Rajkumar Natarajan <rnatarajan@magnite.com>
2026-06-22 17:02:05 -0700

    modularized the nix inputs

--- flake.nix
+++ flake.nix
@@ -1,63 +1,12 @@
 {
-  description = "A very basic flake";
+  description = "NixOS + Home Manager flake with modular Neovim and KDE Plasma config";
+
+  # Nix requires inputs here to be a static attribute set (no import/let).
+  # Input definitions live in path subflakes under flake/inputs/ and are
+  # flattened in outputs via flake/lib.nix.
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    darkly.url = "github:Bali10050/Darkly";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    plasma-manager = {
-      url = "github:pjones/plasma-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
-
-    nvim-config = {
-      url = "github:rajcspsg/nvim";
-      flake = false;
-    };
-    haskell-tools.url = "github:MrcJkb/haskell-tools.nvim";
-    dap-repl-highlights = {
-      url = "github:LiadOz/nvim-dap-repl-highlights";
-      flake = false;
-    };
-    chainsaw = {
-      url = "github:chrisgrieser/nvim-chainsaw";
-      flake = false;
-    };
-    windex = {
-      url = "github:declancm/windex.nvim";
-      flake = false;
-    };
-    nvim-bufterm = {
-      url = "github:boltlessengineer/bufterm.nvim";
-      flake = false;
-    };
-    nvim-smoothcursor = {
-      url = "github:gen740/SmoothCursor.nvim";
-      flake = false;
-    };
-    nvim-tmux-navigation = {
-      url = "github:alexghergh/nvim-tmux-navigation";
-      flake = false;
-    };
-
-    nvim-close-buffers = {
-      url = "github:kazhala/close-buffers.nvim";
-      flake = false;
-    };
-
-    neotest-zig = {
-      url = "github:lawrence-laz/neotest-zig";
-      flake = false;
-    };
-
-    nvim-meow-yarn = {
-      url = "github:retran/meow.yarn.nvim";
-      flake = false;
-    };
+    core.url = "path:./flake/inputs/core";
+    neovim.url = "path:./flake/inputs/neovim";
   };
   outputs =
     {
